dc95f0b81f359c5283f73c94bb139079db3439b8
arun_b <arunbalsen27@gmail.com>
2023-10-15 20:41:31 +0530

    beebox

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,8 @@
   import React, { useEffect, useState } from "react";
   import { Link } from "react-router-dom";
   import { navLinks } from "../constants";
-  import menu from "../assets/menu.svg";
-  import close from "../assets/close.svg";
+  import menu from "../assets";
+  import close from "../assets";
   import logo from "../assets/logo1.png";
   import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
 
